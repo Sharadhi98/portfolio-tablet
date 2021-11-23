92c3b445391da32100ce002c3256e07a4779855c
Sharadhi98 <40205440+Sharadhi98@users.noreply.github.com>
2021-11-23 17:58:47 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /* click event on toggle */
+    $toggleCollapse.click(function(){
+        $nav.toggleClass('collapse');
+    })
+
+    /*text animation*/
+    /* Owl carousel */
+    // $('.owl-carousel').owlCarousel({
+    //     loop:true,
+    //     autoplay:true,
+    //     autoplayTimeout: 3000,
+    // });
+});
+
+//progress bar code
+
